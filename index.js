b2f4a41f9b57359fb94ffb7977d2206907836dd6
Mark George <thingalon@gmail.com>
2021-01-28 00:21:09 +1100

    Better dir layout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const BrowserInterfacePuppeteer = require( './browser-interface-puppeteer' );
-const BrowserInterfaceIframe = require( './browser-interface-iframe' );
-const generateCriticalCSS = require( './generate-critical-css' );
+const BrowserInterfacePuppeteer = require( './lib/browser-interface-puppeteer' );
+const BrowserInterfaceIframe = require( './lib/browser-interface-iframe' );
+const generateCriticalCSS = require( './lib/generate-critical-css' );
 
 module.exports = {
 	BrowserInterfaceIframe,
