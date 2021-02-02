8adcef4a7d3333ca3e7202ed1c28d6444d0a1b9c
Mark George <thingalon@gmail.com>
2021-02-02 16:01:50 +1100

    Add iframe interface tests

---
+++ tests/data/webpack-wrap/main.js
@@ -0,0 +1,4 @@
+/**
+ * Make webpacked Critical CSS Generator lib available in window for testing.
+ */
+window.TestGenerator = require( '../../../index' );
---
+++ tests/lib/test-server.js
@@ -0,0 +1,56 @@
+const express = require( 'express' );
+const webpack = require( 'webpack' );
+const webpackDevMiddleware = require( 'webpack-dev-middleware' );
+
+/**
+ * Test server with webpack middleware, used to test client-side / iframe version
+ * of Critical CSS generation.
+ */
+class TestServer {
+
+	constructor( staticPaths ) {
+		this.port = null;
+		this.app = null;
+		this.middleware = null;
+		this.staticPaths = staticPaths || [];
+	}
+
+	async start() {
+		const compiler = webpack( require( '../data/webpack-wrap/webpack.config.js' ) );
+		this.middleware = webpackDevMiddleware( compiler, { serverSideRender: true } );
+
+		this.app = express();
+		this.app.use( this.middleware );
+
+		for ( const [ virtualPath, realDirectory ] of Object.entries( this.staticPaths ) ) {
+			console.log( virtualPath, realDirectory );
+			this.app.use( '/' + virtualPath, express.static( realDirectory ) )
+		}
+
+		this.app.use( ( req, res ) => res.send( '<html><head><script src="main.min.js"></script></head><body></body></html>' ) );
+
+		return new Promise( ( resolve ) => {
+			const server = this.app.listen( () => {
+				this.port = server.address().port;
+				resolve();
+			} );
+		} );
+	}
+
+	async stop() {
+		if ( this.middleware ) {
+			this.middleware.close();
+		}
+
+		if ( this.app ) {
+			this.app.close();
+		}
+	}
+
+	getUrl() {
+		return 'http://localhost:' + this.port;
+	}
+
+}
+
+module.exports = TestServer;
