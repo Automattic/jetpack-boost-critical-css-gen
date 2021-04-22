ab05226ea37f396b0d1739011b3175aff7e85108
Mark George <thingalon@gmail.com>
2021-04-22 17:09:28 +1000

    Add a CLI tool for generating Critical CSS inside a puppeteer instance

---
+++ bin/generate.js
@@ -0,0 +1,45 @@
+const puppeteer = require( 'puppeteer' );
+const { generateCriticalCSS, BrowserInterfacePuppeteer } = require( '../index' );
+
+async function main() {
+	const urls = process.argv.slice( 2 );
+
+	if ( urls.length === 0 ) {
+		console.log( 'Usage: node bin/generate.js [url1] [url2] ...' );
+	}
+
+	console.log( 'Loading pages: ' );
+	console.log( urls );
+
+	const browser = await puppeteer.launch();
+	const testPages = {};
+	for ( const url of urls ) {
+		testPages[ url ] = await browser.newPage();
+		await testPages[ url ].goto( url );
+	}
+
+	console.log( 'Generating Critical CSS...' );
+
+	const [ css, warnings ] = await generateCriticalCSS({
+		urls,
+		viewports: [
+			{ width: 414, height: 896 },
+			{ width: 1200, height: 800 },
+			{ width: 1920, height: 1080 },
+		],
+		browserInterface: new BrowserInterfacePuppeteer( testPages ),
+	});
+
+	if ( warnings.length ) {
+		console.log( '\n\nwarnings => ' );
+		console.log( warnings );
+	}
+
+	console.log( 'css => ' );
+	console.log( css );
+}
+
+main().catch( err => {
+	console.error( err );
+	process.exit( 1 ); 
+} ).then( () => process.exit( 0 ) );
