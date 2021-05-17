c3699e9290bb9db275557fc68e7d0afb36d3eb84
Mark George <thingalon@gmail.com>
2021-05-17 11:11:09 +1000

    Add tool to bundle library into standalone loadable js lib

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import globals from 'rollup-plugin-node-globals';
+import {terser} from 'rollup-plugin-terser';
+import builtins from 'rollup-plugin-node-builtins';
+
+export default {
+	input: 'index.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'CriticalCSSGenerator',
+		file: 'dist/bundle.js',
+	},
+
+	plugins: [
+		resolve({ browser: true, preferBuiltins: false }),
+		commonjs( {
+			transformMixedEsModules: true
+		} ),
+		globals(),
+		builtins(),
+		json(),
+		terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
