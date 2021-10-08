06b6b19c1e1208aacb26d54a304a4ea6741f0f21
Mark George <thingalon@gmail.com>
2021-10-08 16:05:38 +1100

    Add a version number

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const generateCriticalCSS = require( './lib/generate-critical-css' );
 const { SuccessTargetError } = require( './lib/errors' );
 
 module.exports = {
+	version: '0.0.1',
 	BrowserInterfaceIframe,
 	BrowserInterfacePuppeteer,
 	generateCriticalCSS,
